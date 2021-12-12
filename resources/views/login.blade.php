de58185ee5802bb2a0a2f3a6f67a058668755724
Mohamed Ali <52932544+muhamed3li@users.noreply.github.com>
2021-12-12 23:02:30 +0200

    login done

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,17 +5,17 @@
 <div class="container mt-5 mb-5">
   <div class="row justify-content-center">
     <div class="col-md-7 ">
-      <form>
+      <form action="login" method="POST">
         <div class="form-group">
-          <label for="exampleInputEmail1">Email address</label>
-          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
-          placeholder="Enter email">
+          @csrf
+          <label >Email address</label>
+          <input type="email" name="name" class="form-control" placeholder="Enter email">
         </div>
         <div class="form-group">
-          <label for="exampleInputPassword1">Password</label>
-          <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+          <label >Password</label>
+          <input type="password" name="password" class="form-control"  placeholder="Password">
         </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
+        <button type="submit" class="btn btn-primary">Login</button>
       </form>
     </div>
   </div>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProductController;
+
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +16,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/login', function () {
     return view('login');
 });
+
+Route::post('/login',[UserController::class,'login']);
+Route::get('/',[ProductController::class,'index']);
+
